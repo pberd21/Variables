d8b03ff1e35d56fefa1b0ce808ba9193c08684c3
pberd21 <berdnikov0500@gmail.com>
2024-08-06 21:22:07 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+amount_of_homework = 12
+time_spent = 1.5
+course_name = "Python"
+time_spent_on_one_task = time_spent / amount_of_homework
+print(f"Курс: {course_name}, всего задач: {amount_of_homework}, затрачено часов: {time_spent}, среднее время выполения {time_spent_on_one_task}")
